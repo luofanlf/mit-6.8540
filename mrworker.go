bc7f09481e22ca6ed0758dd9d99b7af9dd4ad6a2
luofan <fan.luo@longbridge-inc.com>
2025-03-13 17:58:59 +0800

    完成map任务结束后将哈希后的健值对写入Nreduce数量的文件中，定义reportMapTask参数结构体

--- mrworker.go
+++ mrworker.go
@@ -10,11 +10,14 @@ package main
 // Please do not change this file.
 //
 
-import "6.5840/mr"
-import "plugin"
-import "os"
-import "fmt"
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+	"plugin"
+
+	"6.5840/mr"
+)
 
 func main() {
 	if len(os.Args) != 2 {
@@ -23,6 +26,8 @@ func main() {
 	}
 
 	mapf, reducef := loadPlugin(os.Args[1])
+
+	fmt.Println("start worker")
 
 	mr.Worker(mapf, reducef)
 }
